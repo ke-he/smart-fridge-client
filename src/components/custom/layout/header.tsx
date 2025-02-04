1ae9dcb6789e6340284fac1a5ec159c081f94999
Kevin Hetzenauer <kevin@hetzenauer.me>
2025-02-04 21:50:41 +0100

    feat(*): settings button

--- src/components/custom/layout/header.tsx
+++ src/components/custom/layout/header.tsx
@@ -1,14 +1,19 @@
-import { Bell, Search } from 'lucide-react';
+import { Bell, Search, Settings } from 'lucide-react';
 import { useRoute } from '@/contexts/route.context';
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
-    const { activeTab } = useRoute();
+  const { activeTab } = useRoute();
+  const router = useRouter();
 
-    return (
-        <header className="flex justify-between items-center p-3 mb-5 w-full ml-0 lg:pl-64 px-4 md:px-6">
-            <Search />
-            <span className="pageTitle">{activeTab?.name.toLowerCase() ?? ''}</span>
-            <Bell />
-        </header>
-    );
+  return (
+    <header className="flex justify-between items-center p-3 mb-5 w-full ml-0 lg:pl-64 px-4 md:px-6">
+      <Search />
+      <span className="pageTitle">{activeTab?.name.toLowerCase() ?? ''}</span>
+      <div className="flex gap-4">
+        <Bell />
+        <Settings onClick={() => router.replace('/settings')} />
+      </div>
+    </header>
+  );
 }
